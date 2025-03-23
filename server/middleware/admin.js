c33142a8e85f400af086425a694a11cb7357df4b
Ganesh Khetawat (Workspace) <ganeshkhetawatworkspace@Ganeshs-MacBook-Air.local>
2025-03-23 07:43:48 +0530

    Initial commit: School Cash Elimination Project

---
+++ server/middleware/admin.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const admin = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    if (user.role !== 'admin') {
+      return res.status(403).send({ error: 'Access denied. Admin privileges required.' });
+    }
+
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+module.exports = admin;
